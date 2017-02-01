e749fa5d4d9b5ba41d3dda80515a0f7e390d6595
gtinon <guillaume.tinon@gmail.com>
2017-02-01 12:14:43 +0100

    Refactor sound management.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+
+    void Awake()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = backgroundMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
